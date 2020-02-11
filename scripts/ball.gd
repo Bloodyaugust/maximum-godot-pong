882c74b2c6bcc30bfaa85dd66cdd090dd587646d
Bloodyaugust <greysonrichey@gmail.com>
2020-02-11 15:03:46 -0700

    More work

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,21 +3,32 @@ extends Node2D
 var BALL_SIZE: float = 16
 var SCREEN_HEIGHT: float = 600
 var SCREEN_WIDTH: float = 1024
+var SPEED: float = SCREEN_WIDTH / 2
 
+onready var _area2d: Area2D = $"Area2D"
 onready var _velocity: Vector2 = Vector2(rand_range(-1, -3), rand_range(-1, 1))
 
+func _on_area2d_entered(area2d):
+  _velocity.x *= -1
+
+func _reset():
+  _velocity = Vector2(rand_range(-1, -3), rand_range(-1, 1))
+  position = Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 func _physics_process(delta):
-  position += _velocity * delta
+  position += _velocity * delta * SPEED
 
 func _process(delta):
   if position.x > SCREEN_WIDTH - (BALL_SIZE / 2):
-    position.x = SCREEN_WIDTH - (BALL_SIZE / 2)
+    _reset()
 
   if position.x < BALL_SIZE / 2:
-    position.x = BALL_SIZE / 2
+    _reset()
 
   if position.y > SCREEN_HEIGHT - (BALL_SIZE / 2):
     position.y = SCREEN_HEIGHT - (BALL_SIZE / 2)
+    _velocity.y *= -1
     
   if position.y < BALL_SIZE / 2:
     position.y = BALL_SIZE / 2
+    _velocity.y *= -1
